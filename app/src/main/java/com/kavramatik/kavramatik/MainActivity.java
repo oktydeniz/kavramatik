0ff9f7d8462ff85a175e5fc0777e775e8c5306b9
oktydeniz <oktydeniz07@gmail.com>
2021-06-17 13:42:23 +0300

    Animasyonlar eklendi, logo duzeltildi.

--- app/src/main/java/com/kavramatik/kavramatik/MainActivity.java
+++ app/src/main/java/com/kavramatik/kavramatik/MainActivity.java
@@ -2,6 +2,9 @@ package com.kavramatik.kavramatik;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.DialogInterface;
 import android.graphics.Color;
@@ -14,6 +17,7 @@ import com.kavramatik.kavramatik.util.SharedPreferencesManager;
 
 public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
     ActionBar actionBar;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +26,15 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
         actionBar = getSupportActionBar();
         ColorDrawable drawable = new ColorDrawable(Color.parseColor("#FF018786"));
         actionBar.setBackgroundDrawable(drawable);
+
         boolean isFirstTime = SharedPreferencesManager.getIsFirstTime(this);
         if (isFirstTime) {
             AppAlertDialogs.showOnlyOnce(MainActivity.this, this);
             SharedPreferencesManager.setIsFirstTime(MainActivity.this, false);
         }
+        navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+
     }
 
     @Override
@@ -36,5 +44,10 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
         } else if (which == DialogInterface.BUTTON_NEGATIVE) {
             IntentsTTS.openTTSSettings(getApplicationContext());
         }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp();
     }
 }
