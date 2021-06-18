0e5b4cf6dc1dca9ce67529c625f0300112cf35d9
oktydeniz <oktydeniz07@gmail.com>
2021-06-18 21:22:14 +0300

    eslestirme ekrani tamamlandi, hatalar duzeltildi

--- app/src/main/java/com/kavramatik/kavramatik/view/education/ColorMatchFragment.java
+++ app/src/main/java/com/kavramatik/kavramatik/view/education/ColorMatchFragment.java
@@ -1,28 +1,41 @@
 package com.kavramatik.kavramatik.view.education;
 
+import android.app.Dialog;
+import android.content.ClipData;
+import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
-import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.kavramatik.kavramatik.R;
 import com.kavramatik.kavramatik.databinding.FragmentColorMatchBinding;
+import com.kavramatik.kavramatik.model.ColorCompModel;
+import com.kavramatik.kavramatik.util.Base64Util;
 import com.kavramatik.kavramatik.viewModel.ColorMatchViewModel;
+
+import java.util.List;
 
 public class ColorMatchFragment extends Fragment {
     private FragmentColorMatchBinding binding;
     private ColorMatchViewModel colorMatchViewModel;
-
-    private static final String TAG = "ColorMatchFragment";
-
+    private int nextModel = 0;
+    private Dialog dialog;
+    MediaPlayer mediaPlayer;
+    private List<ColorCompModel> colorCompModelList;
     public ColorMatchFragment() {
-
     }
 
     @Override
@@ -43,10 +56,45 @@ public class ColorMatchFragment extends Fragment {
         colorMatchViewModel = new ViewModelProvider(requireActivity()).get(ColorMatchViewModel.class);
         colorMatchViewModel.getData();
         observeData();
+        views();
     }
 
-    private void observeData() {
+    private void views() {
+        binding.imageOneMatch.setOnLongClickListener(longClickListener);
+        binding.imageTwoMatch.setOnLongClickListener(longClickListener);
+        binding.mainColor.setOnDragListener(dragListener);
 
+    }
+
+    View.OnLongClickListener longClickListener = v -> {
+        ClipData clipData = ClipData.newPlainText("", "");
+        View.DragShadowBuilder dragShadowBuilder = new View.DragShadowBuilder(v);
+        v.startDrag(clipData, dragShadowBuilder, v, 0);
+        return true;
+    };
+    View.OnDragListener dragListener = (v, event) -> {
+        int dragEvent = event.getAction();
+        switch (dragEvent) {
+            case DragEvent.ACTION_DROP:
+                final View view = (View) event.getLocalState();
+                if (view.getTag().equals(binding.mainColor.getTag())) {
+                    view.setVisibility(View.GONE);
+                    if (nextModel < colorCompModelList.size() - 1) {
+                        nextModel++;
+                    } else {
+                        nextModel = 0;
+                        showFinishDialog();
+                    }
+                    show();
+                }
+
+                break;
+            case DragEvent.ACTION_DRAG_EXITED:
+                break;
+        }
+        return true;
+    };
+    private void observeData() {
         colorMatchViewModel.errorColors.observe(getViewLifecycleOwner(), i -> {
             if (i) {
                 binding.errorTextViewColor.setVisibility(View.VISIBLE);
@@ -58,14 +106,77 @@ public class ColorMatchFragment extends Fragment {
         });
         colorMatchViewModel.mutableLiveDataColors.observe(getViewLifecycleOwner(), models -> {
             if (models.size() >= 1) {
-                Log.i(TAG, "observeData: Model Size :  " + models.size());
+                colorCompModelList = models;
+                show();
             }
         });
+    }
+
+    private void show() {
+        binding.imageOneMatch.setVisibility(View.VISIBLE);
+        binding.imageTwoMatch.setVisibility(View.VISIBLE);
+
+        binding.mainColor.setImageBitmap(setImageFromUrl(colorCompModelList.get(nextModel).getMainColor()));
+        binding.imageOneMatch.setImageBitmap(setImageFromUrl(colorCompModelList.get(nextModel).getColorOne()));
+        binding.imageTwoMatch.setImageBitmap(setImageFromUrl(colorCompModelList.get(nextModel).getColorTwo()));
+
+        binding.mainColor.setTag(colorCompModelList.get(nextModel).getColorTag());
+        binding.imageOneMatch.setTag(colorCompModelList.get(nextModel).getOneTag());
+        binding.imageTwoMatch.setTag(colorCompModelList.get(nextModel).getTwoTag());
+
+    }
+
+    private Bitmap setImageFromUrl(String s) {
+        return Base64Util.decodeToBitmap(s);
+
+    }
+
+    private void showFinishDialog() {
+        play();
+        dialog = new Dialog(requireContext());
+        dialog.setContentView(R.layout.game_over);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        dialog.getWindow().setWindowAnimations(R.style.AnimationForDialogs);
+        Button button = dialog.findViewById(R.id.finished);
+        button.setOnClickListener(v -> {
+            stop();
+            NavController controller = NavHostFragment.findNavController(this);
+            controller.navigate(R.id.action_colorMatchFragment_to_colorFragment);
+            dialog.dismiss();
+        });
+        dialog.show();
+    }
+
+    private void play() {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(requireContext(), R.raw.finish_sound);
+            mediaPlayer.setOnCompletionListener(mp -> stop());
+
+        }
+        mediaPlayer.start();
+    }
+
+    private void stop() {
+        stopPlayer();
+    }
+
+    private void stopPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        stop();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        stop();
     }
 }
